69015ce29b50b1fd280df3791e0e135333cfc234
Tushig Battulga <ehv6@txstate.edu>
2024-09-24 03:15:27 -0500

    Model folder: Post and User classes updated
    Variables ID, content, and date created. As well as getters and setters for the variables.

---
+++ src/main/java/com/example/twitterapp/model/Post.java
@@ -0,0 +1,53 @@
+package com.example.twitterapp.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String content;
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
