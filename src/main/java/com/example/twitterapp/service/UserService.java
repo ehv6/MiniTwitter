51d7b1da61502745ef4bfdf460250b5c2bd7ef7a
Tushig Battulga <ehv6@txstate.edu>
2024-09-24 03:43:20 -0500

    Service: Classes PostService, UserService, and UserServiceImpl updated
    PostService handles saving Post objects with timestamps and retrieving the posts from PostRepository.
    
    UserService updated methods for finding User by username and saving User objects.
    
    UserServiceImpl implements the UserService interface for the use access their account.

---
+++ src/main/java/com/example/twitterapp/service/UserService.java
@@ -0,0 +1,11 @@
+package com.example.twitterapp.service;
+
+import com.example.twitterapp.model.User;
+
+public interface UserService {
+
+    User findByUsername(String username);
+
+    User save(User userDto);
+
+}
