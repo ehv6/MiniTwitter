5b59b0e87d0f67e359137b4fb70c485823e0785e
Tushig Battulga <ehv6@txstate.edu>
2024-09-24 04:51:30 -0500

    Fixed bug for return new Collections<?>
    CustomUserDetails class updated with constructor with 3 parameters.

--- src/main/java/com/example/twitterapp/config/CustomUserDetails.java
+++ src/main/java/com/example/twitterapp/config/CustomUserDetails.java
@@ -11,6 +11,12 @@ public class CustomUserDetails implements UserDetails {
     private String password;
     private Collection<? extends GrantedAuthority> authorities;
 
+    public CustomUserDetails(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
