c85015ea2cc9ee977b08b175a4c8d9e66a0dd40e
Tushig Battulga <ehv6@txstate.edu>
2024-09-24 04:47:28 -0500

    config: CustomUserDetails and CustomUserDetailsService updated
    CustomUserDetails implements the UserDetails interface to provide user authentication information (username, password, authorities, and account status for use in Spring Security).
    
    CustomUserDetailsService  implements interface to load user authentication details from the database

---
+++ src/main/java/com/example/twitterapp/config/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.example.twitterapp.config;
+
+import com.example.twitterapp.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Username or Password is incorrect");
+        }
+        return new CustomUserDetails(user.getUsername(), user.getPassword(), authorities())
+    }
+
+    public Collection<? extends GrantedAuthority> authorities(){
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+}
